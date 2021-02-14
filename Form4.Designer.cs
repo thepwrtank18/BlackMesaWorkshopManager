03cfc97209ae450591e04a8952dfe265979ea43b
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2021-02-14 17:00:22 -0500

    Twelth commit; fixed bug where about page has default icon

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -29,6 +29,7 @@ namespace BlackMesaWorkshopManager
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(aboutForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -145,6 +146,7 @@ namespace BlackMesaWorkshopManager
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(500, 500);
             this.MinimumSize = new System.Drawing.Size(401, 317);
